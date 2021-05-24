4daf39c25b6cd184c11524e49f4fa23acf2c7039
unknown <sirdavid@stanford.edu>
2021-05-24 02:30:26 -0400

    added learning mode to aid practice

--- play.py
+++ play.py
@@ -18,10 +18,14 @@ def commas(num):
     left = left-3
     remain = remain[3:]
   return out
+
+learn = False
 punct = False
-punctYN = str(input("Do you want commas (y/n)? "))
-if (punctYN == 'y'):
+opts = str(input("Options:\ncommas? C\nlearn? L\n")).lower()
+if (opts == 'c' or opts == 'l'):
   punct = True
+if (opts == 'l'):
+  learn = True
 while(game):
   k = int(input("(0 to quit)\n odd root?  "))
   if(k ==0):
@@ -29,10 +33,14 @@ while(game):
     break
   truth = np.random.randint(100)
   out = np.power(truth,k)
+  if learn:
+    for i in range(1,10):
+      print(i*10, 'to the', k, 'is ', commas((i*10)**k))
   if (punct):
     guess = int(input("What's "+commas(out)+" to the 1/"+str(k)+"? "))
   else:
     guess = int(input("What's "+str(out)+" to the 1/"+str(k)+"? "))
+
   ctr+=1
   if guess ==truth:
     ws+=1
